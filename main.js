71f3c72f493dbd7f6db16b85b87c27346637f299
DarioGuastella <jimmyguastella@gmail.com>
2023-11-15 17:28:04 +0100

    feat: primi passi, fine esercizio

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: "Questo testo è inserito tramite Vue!",
+        message2: "Anche la seguente immagine è stata inserita tramite Vue:",
+        photo: "https://picsum.photos/200/300"
       }
     }
   }).mount('#app')
+
+  document.body.style.backgroundColor =  "#ff000080";
+  document.body.style.color = "White";
